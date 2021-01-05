10503236b68b5ba6b8f2d0ef5f806413f6e6c89b
Dave Spina <dspina79@gmail.com>
2021-01-04 23:45:25 -0500

    Reset loading to the content view

--- CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -13,7 +13,7 @@ struct CoreDataProjectApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContraintExampleView()
+            ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
